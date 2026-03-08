da6979f7955af40fd678e508a24973637584d9f2
Dawid20041 <dawid.zmudzki@wp.pl>
2026-03-08 14:58:08 +0100

    Refactor log processing to use Counters for IPs, endpoints, and status codes

--- log-analizer.py
+++ log-analizer.py
@@ -11,25 +11,31 @@ args = parse_arguments()
 
 file_path = args.file
 
-ip_counter = []
-endpoint_counter = []
-status_counter = []
+request_counter = Counter()
+ip_counter = Counter()
+endpoint_counter = Counter()
+status_counter = Counter()
 
 with open(file_path, 'r') as file:
     for line in file:
         clean_line = line.strip()
         if not clean_line:
             continue
-        ip_address, time, method, endpoint, request, status = re.split(r'["\-\[\]\s]+', clean_line)
+        ip_address, timestamp, method, endpoint, request, status = re.split(r'["\-\[\]\s]+', clean_line)
 
-        ip_counter.append(ip_address)
-        endpoint_counter.append(endpoint)
-        status_counter.append(status)
+        request_counter[line] += 1
+        ip_counter[ip_address] += 1
+        endpoint_counter[endpoint] += 1
+        status_counter[status] += 1
 
-        print(f"IP: {ip_address}, Time: {time}, Method: {method}, Endpoint: {endpoint}, Request: {request}, Status: {status}")
+def printing():
+    print(f"Total Requests: {sum(request_counter.values())}\n")
     print(f"Unique IPs: {len(set(ip_counter))}")
     print(f"Unique Endpoints: {len(set(endpoint_counter))}")
     print(f"Unique Status Codes: {len(set(status_counter))}\n")
     print(f"Top 5 IPs: {Counter(ip_counter).most_common(5)}")
     print(f"Top 5 Endpoints: {Counter(endpoint_counter).most_common(5)}")
     print(f"Top 5 Status Codes: {Counter(status_counter).most_common(5)}")
+printing()
+
+    
