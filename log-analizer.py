4eb11c1500fbe62ed70b5916bcc8c87f359e37f6
Dawid20041 <dawid.zmudzki@wp.pl>
2026-03-08 15:11:00 +0100

    Refactor log processing to improve regex matching and error handling

--- log-analizer.py
+++ log-analizer.py
@@ -10,6 +10,8 @@ def parse_arguments():
 args = parse_arguments()
 
 file_path = args.file
+
+pattern = r'(\d+\.\d+\.\d+\.\d+).*\[(.*?)\]\s+"(\w+)\s+([^ ]+).*"\s+(\d{3})'
 errors = []
 
 request_counter = Counter()
@@ -22,7 +24,16 @@ with open(file_path, 'r') as file:
         clean_line = line.strip()
         if not clean_line:
             continue
-        ip_address, timestamp, method, endpoint, request, status = re.split(r'["\-\[\]\s]+', clean_line)
+        match = re.search(pattern, clean_line)
+
+        if not match:
+            continue
+
+        ip_address = match.group(1)
+        timestamp = match.group(2)
+        method = match.group(3)
+        endpoint = match.group(4)
+        status = match.group(5)
         if status.startswith('5') or status.startswith('4'):
             errors.append(clean_line)
 
@@ -36,9 +47,9 @@ def printing():
     print(f"Unique IPs: {len(set(ip_counter))}")
     print(f"Unique Endpoints: {len(set(endpoint_counter))}")
     print(f"Unique Status Codes: {len(set(status_counter))}\n")
-    print(f"Top 5 IPs: {Counter(ip_counter).most_common(5)}")
-    print(f"Top 5 Endpoints: {Counter(endpoint_counter).most_common(5)}")
-    print(f"Top 5 Status Codes: {Counter(status_counter).most_common(5)}")
+    print(f"Top 5 IPs: {ip_counter.most_common(5)}")
+    print(f"Top 5 Endpoints: {endpoint_counter.most_common(5)}")
+    print(f"Top 5 Status Codes: {status_counter.most_common(5)}")
     print(f"Errors: {len(errors)}")
     print(f"Errors Details:")
     for error in errors:
